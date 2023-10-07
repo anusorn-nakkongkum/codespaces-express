5d860a9d829bee92386adc9cefa25595b2babaae
anusorn-nakkongkum <146738950+anusorn-nakkongkum@users.noreply.github.com>
2023-10-07 15:24:48 +0700

    Update index.js add new route

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World 1111!')
-})
+}); 
+
+app.get('/user;, (req, res)=>{
+        res.send({first_name: "anusorn", last_name:"nakkongkum"});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
